e987df54cfe814a7cf481c031dde71ebaa94bee3
hardikivory <hardik1.ivorytechnolab@gmail.com>
2022-07-15 15:08:52 +0530

    second

--- HMS/settings.py
+++ HMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["hostel-management-practice.herokuapp.com", "localhost", "*"]
+ALLOWED_HOSTS = ["hostel-management-project.herokuapp.com", "localhost"]
 
 
 # Application definition
